03ea686d4ac4c43c9ce0c0dc938a6e3bde34f4ab
dong1hyun <limd1238@naver.com>
2022-11-01 15:54:50 +0900

    로그인 완성본

--- app.js
+++ app.js
@@ -52,12 +52,9 @@ app.use(
         name: 'session-cookie'
     }));
 
-app.get('/', (_, res) => {
-    console.log("ddd");
-    res.redirect(301, '/index.html');
-});
+app.get('/', (_, res) => res.redirect(301, '/index.html'));
 app.get('/users', (_, res) => res.send(JSON.stringify(users)))
-app.get('/create', (_, res) => res.redirect(301, '/create.html'));
+app.get('/create', (_, res) => {res.redirect(301, '/create.html')});
 app.get('/read', (_, res) => res.redirect(301, '/read.html'));
 app.get('/update', (_, res) => res.redirect(301, '/update.html'));
 app.get('/delete', (_, res) => res.redirect(301, '/delete.html'));
@@ -65,44 +62,49 @@ app.get('/delete', (_, res) => res.redirect(301, '/delete.html'));
 
 // 사용자 정보 추가
 app.post('/cid', upload.single('image'), (req, res) => {
-    console.log("사용자 정보 추가")
     const { id, name, birth, gender } = req.body;
-    users[id] = { name, birth, gender, 'img': req.file?.path ?? '' };
-    res.redirect(301, '/index.html');
+    if(id in users){  //이미 존재하는 아이디인 경우
+        res.send("<h1>이미 생성된 아이디입니다.</h1>");
+    }
+    else{  //없는 아이디인 경우
+        users[id] = { name, birth, gender, 'img': req.file?.path ?? '' };
+        res.redirect(301, '/index.html');
+    }
 });
 
 // 사용자 정보 조회
 app.get('/rid', (req, res) => {
-    console.log("사용자 정보 조회")
     const id = req?.query?.id;
-    res.send(id in users ? JSON.stringify(users[id]) : `존재하지 않은 ID: ${id}`);
+    res.send(id in users ? JSON.stringify(users[id]) : `<h1>존재하지 않은 ID: ${id}</h1>`);
 });
 
 // 사용자 정보 수정
 app.post('/uid', upload.single('image'), (req, res) => {
-    console.log("사용자 정보 수정")
-    const { id, name, birth, gender } = req.body;
+    let { id, name, birth, gender } = req.body;
+    let imgPath;
     if (id in users) {
-        users[id] = { name, birth, gender, 'img': req.file?.path ?? '' };
+        //수정 정보를 입력하지 않으면 기존 정보를 그대로 가져감
+        if (name === '') name = users[id].name;  
+        if (birth === '') birth = users[id].birth;
+        if (gender === '') gender = users[id].gender;
+        imgPath = req.file?.path ?? '';
+        users[id] = { name, birth, gender, 'img': imgPath };
         res.redirect(301, '/index.html');
-    }
-    else {
-        res.send(`존재하지 않은 ID: ${id}`);
-    }
+    } else
+        res.send(`<h1>존재하지 않은 ID: ${id}</h1>`);
 })
 
-
 // 사용자 정보 삭제
-app.get('/did', (req, res) => { 
-    console.log("사용자 정보 삭제");
+app.get('/did', (req, res) => {
     const id = req?.query?.id;
-    if(id in users){
-        delete users[id];
-        res.redirect(301, '/index.html');
-    }
-    else{
-        res.send(`존재하지 않은 ID: ${id}`);
-    }
+    if (id in users) {
+        fs.unlink(users[id].img, (err)=>{
+            console.error(err);
+            delete users[id];
+            res.redirect(301, '/index.html');
+        })
+    } else
+        res.send(`<h1>존재하지 않은 ID: ${id}</h1>`);
 });
 
 app.listen(app.get('port'), () => console.log(`${app.get('port')} 번 포트에서 대기 중`));
